2aefdb179548b1f18771876e2a4a69c4f7ce572d
Olha-Starykova <olenkalog@gmail.com>
2021-04-06 20:19:52 +0300

    Updates

---
+++ static/js/main.e1dcfc79.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),i=c(3),o=c.n(i),s=c(0);var a=function(){return Object(s.jsx)("div",{children:"\u041f\u0440\u0438\u0432\u0435\u0442"})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.e1dcfc79.chunk.js.map
